5ed8f0eb6625fd757cadaa32497852f74b48b710
Hyeonseo <54432870+hyeonseo0128@users.noreply.github.com>
2022-12-08 18:00:35 +0900

    Update msw_lidar_distance.js

--- msw_lidar_distance.js
+++ msw_lidar_distance.js
@@ -99,7 +99,7 @@ function runLib(obj_lib) {
 
         let run_lib = spawn(scripts_arr[0], scripts_arr.slice(1));
 
-        run_lib.stdout.on('data', functㄴㄴㄴㄴion (data) {
+        run_lib.stdout.on('data', function (data) {
             console.log('stdout: ' + data);
         });
 
